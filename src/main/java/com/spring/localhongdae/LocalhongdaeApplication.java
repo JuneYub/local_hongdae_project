739c800a3890a6e8bd18903505f762100afe9aca
JuneYub <ajsjdlwj0123@gmail.com>
2023-06-28 21:01:11 +0900

    Initial commit

---
+++ src/main/java/com/spring/localhongdae/LocalhongdaeApplication.java
@@ -0,0 +1,16 @@
+package com.spring.localhongdae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class LocalhongdaeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalhongdaeApplication.class, args);
+    }
+
+}

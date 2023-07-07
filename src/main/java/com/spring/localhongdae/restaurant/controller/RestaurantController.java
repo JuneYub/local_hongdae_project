ed94e717e9e758f1afebed98265b3bf2aa600996
JuneYub <ajsjdlwj0123@gmail.com>
2023-07-07 20:09:57 +0900

    feat: Implementation of the restaurant list card title image function

--- src/main/java/com/spring/localhongdae/restaurant/controller/RestaurantController.java
+++ src/main/java/com/spring/localhongdae/restaurant/controller/RestaurantController.java
@@ -2,12 +2,15 @@ package com.spring.localhongdae.restaurant.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class RestaurantController {
 
     @GetMapping("/restaurant/list")
-    public String getList() {
-        return "restaurant-list";
+    public ModelAndView getList() {
+        ModelAndView model = new ModelAndView();
+        model.setViewName("restaurant/restaurant-list");
+        return model;
     }
 }

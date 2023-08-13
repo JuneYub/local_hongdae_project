c06ce07eb0264be9c819fbfd5baa822fcd217082
JuneYub <ajsjdlwj0123@gmail.com>
2023-08-13 17:26:48 +0900

    feat: [] #comment Implementing find restaurant list, paging process - 23.08.13 #close #time 4h +review SR @JuneYub

--- src/main/java/com/spring/localhongdae/restaurant/controller/RestaurantController.java
+++ src/main/java/com/spring/localhongdae/restaurant/controller/RestaurantController.java
@@ -2,9 +2,14 @@ package com.spring.localhongdae.restaurant.controller;
 
 import com.spring.localhongdae.entity.City;
 import com.spring.localhongdae.entity.District;
+import com.spring.localhongdae.entity.Place;
 import com.spring.localhongdae.restaurant.service.RestaurantService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,11 +25,22 @@ public class RestaurantController {
     RestaurantService restaurantService;
 
     @GetMapping("/restaurant/list")
-    public ModelAndView getResturantList() {
+    public ModelAndView getResturantList(HttpServletRequest request, @PageableDefault(page = 1, size = 5)Pageable pageable) {
         ModelAndView model = new ModelAndView();
         List<City> cities = restaurantService.getCities();
+        Page<Place> placePage = restaurantService.getPageByDistrictId(request, pageable);
 
+        // 페이지 바 만들기
+        int blockSize = 5;
+        int startPage = (((int) Math.ceil(((double) pageable.getPageNumber() / blockSize))) - 1) * blockSize + 1;
+        int endPage = Math.min(startPage + blockSize - 1, placePage.getTotalPages());
+
+        model.addObject("selectedCity",request.getParameter("selectedCity"));
+        model.addObject("selectedDistrict",request.getParameter("selectedDistrict"));
         model.addObject("cities", cities);
+        model.addObject("placePage", placePage);
+        model.addObject("startPage", startPage);
+        model.addObject("endPage", endPage);
         model.setViewName("restaurant/restaurant-list");
         return model;
     }
@@ -42,4 +58,5 @@ public class RestaurantController {
         List<District> districts = restaurantService.getDistricts(cityId);
         return districts;
     }
+
 }

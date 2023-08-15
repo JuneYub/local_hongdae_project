ad2938a9c1ba4912ba7288cc5e5463b4b1f83537
JuneYub <ajsjdlwj0123@gmail.com>
2023-08-15 23:21:10 +0900

    feat: [] #comment Implement Marker Marking on Map - 23.08.15 #close #time 1h +review SR @JuneYub

--- src/main/java/com/spring/localhongdae/restaurant/controller/RestaurantController.java
+++ src/main/java/com/spring/localhongdae/restaurant/controller/RestaurantController.java
@@ -30,17 +30,21 @@ public class RestaurantController {
         List<City> cities = restaurantService.getCities();
         Page<Place> placePage = restaurantService.getPageByDistrictId(request, pageable);
 
-        // 페이지 바 만들기
-        int blockSize = 5;
-        int startPage = (((int) Math.ceil(((double) pageable.getPageNumber() / blockSize))) - 1) * blockSize + 1;
-        int endPage = Math.min(startPage + blockSize - 1, placePage.getTotalPages());
+        if(placePage != null) {
+            // 페이지 바 만들기
+            int blockSize = 5;
+            int startPage = (((int) Math.ceil(((double) pageable.getPageNumber() / blockSize))) - 1) * blockSize + 1;
+            int endPage = Math.min(startPage + blockSize - 1, placePage.getTotalPages());
+            model.addObject("startPage", startPage);
+            model.addObject("endPage", endPage);
+        }
+
 
         model.addObject("selectedCity",request.getParameter("selectedCity"));
         model.addObject("selectedDistrict",request.getParameter("selectedDistrict"));
         model.addObject("cities", cities);
         model.addObject("placePage", placePage);
-        model.addObject("startPage", startPage);
-        model.addObject("endPage", endPage);
+
         model.setViewName("restaurant/restaurant-list");
         return model;
     }
